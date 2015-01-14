d011fab0fc1d51958b341f9e9053173490579267
wbond <will@wbond.net>
2015-01-14 09:48:48 -0500

    Added classifiers

--- setup.py
+++ setup.py
@@ -11,5 +11,16 @@ setup(
     author="wbond",
     author_email="will@wbond.net",
     license="MIT License",
-    packages=["pymeta"]
+    packages=["pymeta"],
+    classifiers = [
+        'Development Status :: 3 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4'
+    ]
     )
